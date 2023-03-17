14ba57ec154accb3e4c886d350e4c436074e4004
120090162 <85002296+120090162@users.noreply.github.com>
2023-03-18 03:54:35 +0800

    add sub

--- alu_test/ALU.v
+++ alu_test/ALU.v
@@ -1,46 +1,89 @@
-module alu(i_datain,gr1,c);
+module alu(
+        input wire[31:0] instruction,
+        input wire[31:0] regA, // the address of regA is 00000, the address of regB is 00001
+        input wire[31:0] regB,
 
-output signed[31:0] c;
-//output zero;
-//output overflow;
-//output neg;
+        output reg[31:0] result,
+        output reg[2:0] flags  // the first bit is zero flag, the second bit is negative flag, the third bit is overflow flag.
+    );
 
-input signed[31:0] i_datain,gr1;
+    reg[5:0] opcode;
+    reg[5:0] func;
+    reg[4:0] rs;
+    reg[4:0] rt;
+    reg[4:0] rd;
+    reg[4:0] sa;
+    reg[15:0] imm16;
 
-reg[5:0] opcode, func;
+    // R-Type: 000000
+    // add, addu, sub, subu, and, nor, or, xor, slt, sltu, sll, sllv, srl, srlv, sra, srav
 
-reg[31:0] reg_C;
-//reg zf;
-//reg nf;
-reg[31:0] reg_A, reg_B;
+    // I-Type: except 000000, 00001x, 0100xx
+    // addi, addiu, andi, ori, xori, beq, bne, slti, sltiu, lw, sw
 
-parameter gr0 = 32'h0000_0000;
+    // J-Type: 00001x
 
+    always @(*)
+    begin // parse the opcode to check the type of instruction
+        opcode <= instruction[31:26];
+        func <= instruction[5:0];
+        imm16 <= instruction[15:0];
+        rs <= instruction[25:21];
+        rt <= instruction[20:16];
+        rd <= instruction[15:11];
+        sa <= instruction[10:6];
+    end
 
-always @(i_datain,gr1)
-begin
-
-opcode = i_datain[31:26];
-func = i_datain[5:0];
-
-case(opcode)
-6'b000000:
-begin
-
-case(func)
-6'b000000:
-begin
-reg_A = gr1;
-reg_B = i_datain[10:6];
-reg_C = reg_A << reg_B;
-end
-endcase
-
-end
-endcase
-
-end
-
-assign c = reg_C[31:0];
-
+    always @(*)
+    begin // check the type and fetch values in mem
+        if(opcode == 6'b000000) // R: reg[rd] <- reg[rs] op reg[rt]
+        begin
+            if(func == 6'b100000)
+            begin // add(overflow)
+                result <= regA + regB;
+                if(regA[31] == regB[31])
+                begin
+                    flags[0] <= result[31] ^ regA[31];
+                end
+                else
+                    flags[0] <= 1'b1;
+                flags[2:1] <= {result == 32'b0, result[31]};
+            end
+            else if(func == 6'b100001) // addu
+            begin // addu
+                result <= regA + regB;
+                flags[0] <= 1'b0;
+                flags[2:1] <= {result == 32'b0, result[31]};
+            end
+            else if(func == 6'b100010) begin // sub(overflow)
+                if(rs == 5'b0)begin
+                    result <= regA + ~regB + 1;
+                end
+                else begin
+                    result <= regB + ~regA + 1;
+                end
+                if(regA[31] == regB[31])
+                begin
+                    flags[0] <= result[31] ^ regA[31];
+                end
+                else
+                    flags[0] <= 1'b1;
+                flags[2:1] <= {result == 32'b0, result[31]};
+            end
+            else if(func == 6'b100011) begin // sub
+                if(rs == 5'b0)begin
+                    result <= regA + ~regB + 1;
+                end
+                else begin
+                    result <= regB + ~regA + 1;
+                end
+                flags[0] <= 1'b0;
+                flags[2:1] <= {result == 32'b0, result[31]};
+            end
+        end
+        else // I: reg[rt] <- reg[rs] op imm
+        begin
+            ;
+        end
+    end
 endmodule
